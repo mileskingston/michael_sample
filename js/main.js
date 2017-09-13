55e17cecc7e1786a9aeb3e34421e4d6485ecdf67
Miles Kingston <miles.kingston@dixonscarphone.com>
2017-09-13 14:20:17 +0100

    updated header

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
+window.addEventListener('resize', setBannerHeight);
+// window.addEventListener('scroll', stickyHeader);
+
 function setBannerHeight() {
   var windowHeight = window.innerHeight;
   document.querySelector('#banner .parallax-window').style.height = windowHeight + 'px';
-  console.log(windowHeight);
 };
 setBannerHeight();
 
-window.addEventListener('resize', setBannerHeight);
+// function stickyHeader() {
+//   var navbar = document.querySelector('.navbar');
+//   var navHeight = navbar.offsetHeight;
+  
+//   console.log(navHeight);
+//   console.log(navbar.pageYOffset);
+//   console.log(navbar.scrollTop);
+// };
+
+var h = document.querySelector('.navbar');
+var stuck = false;
+var stickPoint = getDistance();
+
+function getDistance() {
+  var topDist = h.offsetTop;
+  return topDist;
+}
+
+window.onscroll = function(e) {
+  var distance = getDistance() - window.pageYOffset;
+  var offset = window.pageYOffset;
+  if ((distance <= 0) && !stuck) {
+    h.style.position = 'fixed';
+    h.style.top = '0px';
+    stuck = true;
+  } else if (stuck && (offset <= stickPoint)){
+    h.style.position = 'static';
+    stuck = false;
+  }
+}
